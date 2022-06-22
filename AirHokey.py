6f88f3b5371397007228d8062bb3a94087d95ae2
RUPAL DEWANGAN <dewangan1501@gmail.com>
2022-06-22 20:48:38 +0530

    22.06.2022
    Starter Challenge

---
+++ AirHokey.py
@@ -0,0 +1,4 @@
+t=int(input())
+for in range(t):
+    a,b=map(int, input().split())
+    print(7-(max(a,b)))
--- FirstAndLast.py
+++ FirstAndLast.py
@@ -5,12 +5,5 @@ for i in range(t):
     if n==2:
         print(a[0]+a[1])
     else:
-        while True:
-            if a[len-2]>a[len-1]:
-                temp=a[0]
-                a[0]=a[len-1]
-                for i in range(1,len-1):
-                    a[i+1]=a[i]
-                a[i]=temp
-            break
-        print(a[0]+a[len-1])   
+                      
+        print(a[0]+a[len(a)-1])   
